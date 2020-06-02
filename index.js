5e70260078723629a1de5ac9bbe32e8340e14225
nzws <dev@nzws.me>
2020-06-02 13:08:22 +0900

    refactor, fix error

--- index.js
+++ index.js
@@ -26,12 +26,11 @@ module.exports = app => {
         context.payload.pull_request.number;
 
       const timeStamp = Math.floor(new Date().getTime() / 1000);
-      if (cooltime[name] && timeStamp - cooltime[name] < 5) {
-        cooltime[name] = timeStamp;
+      cooltime[name] = timeStamp;
+      if (cooltime[name] && timeStamp - cooltime[name] < 10) {
         app.log('[COOL TIME]', name);
         return;
       }
-      cooltime[name] = timeStamp;
 
       // check all labels
       const labels = context.payload.pull_request.labels.map(
@@ -44,13 +43,11 @@ module.exports = app => {
         app.log('[CONFIG IS NOT FOUND]', name);
         return;
       }
-      app.log(config.delete_branch === 'delete');
+
       let isExist = false;
       config.labels.forEach(label => {
         if (Array.isArray(label)) {
-          const result = label.every(value => {
-            return labels.indexOf(value) !== -1;
-          });
+          const result = label.every(value => labels.indexOf(value) !== -1);
           if (result) isExist = true;
         } else {
           if (labels.indexOf(label) !== -1) isExist = true;
